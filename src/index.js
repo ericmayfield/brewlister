6ec3502c9efdc7b8aa71ac758cedf52b5a29a10e
Eric Mayfield <eric@visiblycreative.com>
2019-06-11 09:00:15 -0400

    Initial imports of react/redux libraries

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const enhancers = [applyMiddleware(thunk)]
+const composedEnhancers = composeWithDevTools(...enhancers)
+
+const store = createStore(rootReducer, undefined, composedEnhancers)
+
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>
+    , document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
